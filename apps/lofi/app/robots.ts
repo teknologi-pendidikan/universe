96af9416d3129af24af6948574b70f338a529f91
vzrenggamani <rengga.prakoso@gmail.com>
2023-07-02 02:40:16 +0700

    fix(lofi): allow bot to crawl _next

--- apps/lofi/app/robots.ts
+++ apps/lofi/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/api/', '/_next/', '/admin/', '/sso/', '/login/'],
+      disallow: ['/api/', '/admin/', '/sso/', '/login/'],
     },
     sitemap: 'https://lofi.teknologipendidikan.or.id/sitemap.xml',
   }
