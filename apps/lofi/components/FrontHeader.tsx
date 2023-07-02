c2d65c49227932506a50ecc78a8ae892f9de1029
vzrenggamani <rengga.prakoso@gmail.com>
2023-07-02 10:28:30 +0700

    chore: make the link programtically

--- apps/lofi/components/FrontHeader.tsx
+++ apps/lofi/components/FrontHeader.tsx
@@ -1,28 +1,71 @@
 import LogoLOFI from 'assets/logo-lofi.svg'
-import Image from "next/image"
+import Image from 'next/image'
+import Link from 'next/link'
 
-export default function FrontHeader() {
+type Props = {
+  title?: string
+}
+
+const LeftLink = [
+  {
+    title: 'Opini',
+    link: '/category/opini',
+  },
+  {
+    title: 'Referensi Informasi',
+    link: '/category/referensi',
+  },
+  {
+    title: 'Prestasi',
+    link: '/category/prestasi',
+  },
+]
+
+const RightLink = [
+  {
+    title: 'Event',
+    link: 'https://with.teknologipendidikan.or.id',
+  },
+  {
+    title: 'Institute',
+    link: 'https://institute.teknologipendidikan.or.id',
+  },
+  {
+    title: 'Research Center',
+    link: 'https://research.teknologipendidikan.or.id',
+  },
+]
+
+export default function FrontHeader({
+  title = 'Majalah dan Surat Kabar Teknologi Pendidikan',
+}: Props) {
   return (
     <section
       id="frontheader"
       className="container mx-auto px-4 my-10 max-w-screen-xl pt-12"
     >
-      <div className="flex flex-row justify-around items-center">
+      <div className="flex flex-row justify-around items-center text-brandblue-800">
         <div className="hidden lg:flex justify-between space-x-12 items-center">
-          <p>Riset</p>
-          <p>Pendidikan</p>
-          <p>Kebijakan</p>
+          {LeftLink.map((link) => (
+            <Link href={link.link} className="hover:underline">
+              <p>{link.title}</p>
+            </Link>
+          ))}
         </div>
         <div className="md:col-span-2 justify-center items-center">
-          <Image src={LogoLOFI} alt="Logo LOFI" width={200} height={200} />
+          <Link href="/">
+            <Image src={LogoLOFI} alt="Logo LOFI" width={200} height={200} />
+          </Link>
         </div>
         <div className="hidden lg:flex justify-between space-x-12 items-center">
-          <p>Event</p>
-          <p>Institute</p>
-          <p>Publikasi</p>
+          {RightLink.map((link) => (
+            <Link href={link.link} className="hover:underline">
+              <p>{link.title}</p>
+            </Link>
+          ))}
         </div>
       </div>
-      <h1 className="text-center w-full pt-12 text-2xl font-semibold">Redaksi EDTECH-ID | Majalah dan Surat Kabar Teknologi Pendidikan</h1>
+      <h1 className="text-center w-full font-serif pt-12 text-4xl">{title}</h1>
     </section>
   )
 }
