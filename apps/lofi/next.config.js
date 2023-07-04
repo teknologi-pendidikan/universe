e503bb38aaca9975057a3f24ac2e15884adbaaad
vzrenggamani <rengga.prakoso@gmail.com>
2023-07-04 08:18:01 +0700

    feat: add netlify cms

--- apps/lofi/next.config.js
+++ apps/lofi/next.config.js
@@ -10,13 +10,13 @@ const nextConfig = {
         headers: nextSafe({
           isDev: false,
           contentSecurityPolicy: {
-            'connect-src': "'self' webpack://* https: data:",
+            'connect-src': "'self' webpack://* https: data: http://localhost:8081/api/v1",
             'frame-src':
               "'none' https://www.youtube.com/ https://docs.google.com/",
             'img-src':
-              "'self' https://is3.cloudhost.id/teknologipendidikan/ https://drive.google.com https://*.googleusercontent.com https://www.netlify.com/img/",
+              "'self' blob: https://*.teknologipendidikan.or.id https://*.githubusercontent.com https://is3.cloudhost.id/teknologipendidikan/ https://drive.google.com https://*.googleusercontent.com https://www.netlify.com/img/",
             'script-src':
-              "'self' 'unsafe-eval' 'unsafe-inline' https://www.youtube.com/ https://www.googletagmanager.com/gtag/js https://netlify-rum.netlify.app/",
+              "'self' 'unsafe-eval' 'unsafe-inline' https://www.youtube.com/ https://www.googletagmanager.com/gtag/js https://netlify-rum.netlify.app/ https://unpkg.com/netlify-cms@^2.10.17/dist/",
             'style-src': "'self' 'unsafe-inline'",
             'worker-src': "'self' blob:",
             'report-uri': 'https://dptsi.edtech.or.id',
