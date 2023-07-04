e702ac55471709428b956b0d59b6d0fa1a3af90f
vzrenggamani <rengga.prakoso@gmail.com>
2023-07-04 09:29:50 +0700

    refactor: split admin route csp rule

--- apps/lofi/next.config.js
+++ apps/lofi/next.config.js
@@ -10,7 +10,26 @@ const nextConfig = {
         headers: nextSafe({
           isDev: false,
           contentSecurityPolicy: {
-            'connect-src': "'self' webpack://* https: data: http://localhost:8081/api/v1",
+            'connect-src': "'self' webpack://* https: data:",
+            'frame-src':
+              "'none' https://www.youtube.com/ https://docs.google.com/",
+            'img-src':
+              "'self' blob: https://*.teknologipendidikan.or.id https://is3.cloudhost.id/teknologipendidikan/ https://drive.google.com https://*.googleusercontent.com https://www.netlify.com/img/",
+            'script-src':
+              "'self' 'unsafe-eval' 'unsafe-inline' https://www.youtube.com/ https://www.googletagmanager.com/gtag/js https://netlify-rum.netlify.app/",
+            'style-src': "'self' 'unsafe-inline'",
+            'worker-src': "'self' blob:",
+            'report-uri': 'https://dptsi.edtech.or.id',
+          },
+        }),
+      },
+      {
+        source: '/admin/:path*',
+        headers: nextSafe({
+          isDev: false,
+          contentSecurityPolicy: {
+            'connect-src':
+              "'self' webpack://* https: data: blob: http://localhost:8081/api/v1",
             'frame-src':
               "'none' https://www.youtube.com/ https://docs.google.com/",
             'img-src':
