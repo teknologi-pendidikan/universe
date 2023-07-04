c5da13d03d446802cb34ef70ce0dddebca0d3321
vzrenggamani <rengga.prakoso@gmail.com>
2023-07-05 06:00:43 +0700

    fix: delete eval on script directive csp

--- apps/lofi/next.config.js
+++ apps/lofi/next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
             'img-src':
               "'self' blob: https://*.teknologipendidikan.or.id https://is3.cloudhost.id/teknologipendidikan/ https://drive.google.com https://*.googleusercontent.com https://www.netlify.com/img/",
             'script-src':
-              "'self' 'unsafe-eval' https://www.googletagmanager.com/gtag/js https://netlify-rum.netlify.app/",
+              "'self' https://www.googletagmanager.com/gtag/js https://netlify-rum.netlify.app/",
             'style-src': "'self' 'unsafe-inline'",
             'worker-src': "'self' blob:",
             'report-uri': 'https://dptsi.edtech.or.id',
