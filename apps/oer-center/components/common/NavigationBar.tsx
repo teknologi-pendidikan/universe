568c697306a5c5a3ff5234b22f7d02cf73df998d
vzrenggamani <rengga.prakoso@gmail.com>
2023-07-12 18:23:34 +0700

    feat: make the link for interactive and delete unsuede script

--- apps/oer-center/components/common/NavigationBar.tsx
+++ apps/oer-center/components/common/NavigationBar.tsx
@@ -8,27 +8,8 @@ import { usePathname } from 'next/navigation'
 export default function NavigationBar() {
   const pathname = usePathname()
   return (
-    <header
-      id="component-layout-header"
-      className="fixed inset-x-0 top-0 z-50 w-full bg-white shadow-md"
-      onScroll={
-        // hide global navigation when scrolling down
-        () => {
-          const globalNavigation = document.getElementById('global-navigation')
-          if (globalNavigation) {
-            if (window.scrollY > 0) {
-              globalNavigation.classList.add('hidden')
-            } else {
-              globalNavigation.classList.remove('hidden')
-            }
-          }
-        }
-      }
-    >
-      <div
-        id="global-navigation"
-        className="container mx-auto flex items-center justify-between py-5"
-      >
+    <header id="component-layout-header" className="fixed inset-x-0 top-0 z-50 w-full bg-white shadow-md">
+      <div id="global-navigation" className="container mx-auto flex items-center justify-between py-5">
         <div className="flex items-center">
           <Link href="/" className="flex items-center">
             <Image
@@ -47,8 +28,8 @@ export default function NavigationBar() {
               href={item.url}
               className={
                 pathname === item.url
-                  ? 'text-gray-800 underline underline-offset-8 transition duration-300 hover:text-gray-600'
-                  : 'text-gray-800 transition duration-300 hover:text-gray-600'
+                  ? 'text-gray-800 underline underline-offset-8 transition duration-300 hover:font-semibold hover:text-gray-600'
+                  : 'text-gray-800 transition duration-300 hover:font-semibold hover:text-gray-600'
               }
             >
               {item.name}
@@ -56,13 +37,18 @@ export default function NavigationBar() {
           ))}
         </nav>
       </div>
-      <div
-        id="content-navigation"
-        className="item-start flex justify-between bg-brandblue py-5"
-      >
+      <div id="content-navigation" className="item-start flex justify-between bg-brandblue py-5">
         <nav className="container mx-auto flex items-center justify-start space-x-8 text-xl font-bold text-white">
           {ContentNavigation.map((item) => (
-            <Link key={item.url} href={item.url}>
+            <Link
+              key={item.url}
+              href={item.url}
+              className={
+                pathname === item.url
+                  ? 'text-white underline underline-offset-8 transition duration-300 hover:text-brandblue-800'
+                  : 'text-white transition duration-300 hover:text-brandblue-800'
+              }
+            >
               {item.name}
             </Link>
           ))}
