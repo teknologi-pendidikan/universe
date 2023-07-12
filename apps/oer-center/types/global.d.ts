3f3f9f4b0774c4ff13d7cfcf0b9248aef0794d5e
vzrenggamani <rengga.prakoso@gmail.com>
2023-07-12 18:21:31 +0700

    chore: thematic content properties

--- apps/oer-center/types/global.d.ts
+++ apps/oer-center/types/global.d.ts
@@ -15,6 +15,28 @@ declare global {
       uuid: string
     }
   }
+
+  type uuid = string
+  type contentType = 'video' | 'slides' | 'image' | 'text'
+
+  interface ThematicLearningContentProperties {
+    uuid: string
+    thematicTitle: string
+    thematicDescription: string
+    thematicThumbnail: string
+    thematicLearningContents: Array<uuid>
+  }
+
+  interface ThematicLearningContentPageViewProps {
+    params: {
+      uuid: string
+    }
+  }
 }
 
-export type { LearningContentPageViewProps, LearningContentProperties }
+export type {
+  LearningContentPageViewProps,
+  LearningContentProperties,
+  ThematicLearningContentPageViewProps,
+  ThematicLearningContentProperties,
+}
