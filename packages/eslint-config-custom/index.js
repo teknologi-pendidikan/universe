60f89eea9d90277a0fe551a50ef38e1a06551c91
vzrenggamani <rengga.prakoso@gmail.com>
2023-07-12 13:56:32 +0700

    chore: tweaking eslint for development

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,15 +1,20 @@
 module.exports = {
-  extends: ["next/core-web-vitals", "turbo", "prettier"],
+  extends: ['next/core-web-vitals', 'turbo', 'prettier'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "no-console": "error",
-    "no-unused-vars": "error",
-    "no-debugger": "error",
-    "no-undef": "error",
+    '@next/next/no-html-link-for-pages': 'off',
+    'no-console': [
+      'error',
+      {
+        allow: ['error', 'warn'],
+      },
+    ],
+    'no-unused-vars': ['error', { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }],
+    'no-debugger': 'error',
+    'no-undef': 'error',
   },
   parserOptions: {
     babelOptions: {
-      presets: [require.resolve("next/babel")],
+      presets: [require.resolve('next/babel')],
     },
   },
-};
+}
