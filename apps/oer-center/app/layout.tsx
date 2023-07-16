ceaed4ca3be7be9439be40abab15e6f13ab3d46d
vzrenggamani <rengga.prakoso@gmail.com>
2023-07-16 17:45:53 +0700

    feat: basic SEO metadata from globalkonten

--- apps/oer-center/app/layout.tsx
+++ apps/oer-center/app/layout.tsx
@@ -1,5 +1,6 @@
 import NavigationBar from 'components/common/NavigationBar'
 import GoogleAnalytics from 'components/libs/GoogleAnalytics'
+import GlobalSiteContent from 'data/site.content.json'
 import type { Metadata } from 'next'
 import React from 'react'
 
@@ -7,9 +8,39 @@ import React from 'react'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'OER Center - EDTECH-ID',
-  description:
-    'Open Educational Resource Center is Open Educational Resources managed and operated by Teknologi Pendidikan ID. This OER mainly focused on professional development in scope of educational technology proficiency.',
+  title: GlobalSiteContent.globalContentData.title,
+  description: GlobalSiteContent.globalContentData.productDescription,
+  manifest: '/manifest.json',
+  creator: GlobalSiteContent.globalContentData.creator,
+  themeColor: '#FFD026',
+  keywords: [
+    'oer',
+    'open educational resources',
+    'edtech',
+    'edtech-id',
+    'teknologi pendidikan',
+    'teknologi pendidikan id',
+    'teknologi pendidikan indonesia',
+  ],
+  viewport: 'width=device-width, initial-scale=1.0',
+  robots: 'index, follow',
+  openGraph: {
+    type: 'website',
+    locale: 'id_ID',
+    url: GlobalSiteContent.globalContentData.url,
+    siteName: GlobalSiteContent.globalContentData.title,
+    title: GlobalSiteContent.globalContentData.title,
+    description: GlobalSiteContent.globalContentData.productDescription,
+    images: [
+      {
+        url: 'https://sumberbelajar.teknologipendidikan.or.id/opengraph-image.png',
+        width: 1200,
+        height: 630,
+        alt: GlobalSiteContent.globalContentData.title,
+      },
+    ],
+  },
+  abstract: GlobalSiteContent.globalContentData.description,
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
