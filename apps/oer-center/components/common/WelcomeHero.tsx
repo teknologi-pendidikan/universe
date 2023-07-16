59106222d212a1c9de4d4cf4f2a7800739d8f14d
vzrenggamani <rengga.prakoso@gmail.com>
2023-07-16 18:04:08 +0700

    feat: hide welcome header on mobile

--- apps/oer-center/components/common/WelcomeHero.tsx
+++ apps/oer-center/components/common/WelcomeHero.tsx
@@ -3,32 +3,29 @@ import Image from 'next/image'
 
 export default function WelcomeHero() {
   return (
-    <section id="welcome-hero" className="relative">
+    <section id="welcome-hero" className="hidden lg:relative">
       <Image
         src="/welcome-hero-bg.png"
         alt="Welcome Hero Background"
-        className="h-screen max-h-screen w-full object-cover"
+        className="invisible h-screen max-h-screen w-full object-cover lg:block"
         width={1920}
         height={1080}
         quality={75}
       />
       <div id="hero-content" className="absolute bottom-10 w-full">
-        <div className="flex max-w-2xl flex-col items-start justify-start space-y-4 p-12">
+        <div className="flex max-w-2xl flex-col items-start justify-start space-y-4 pt-32 lg:p-12">
           <h1 className="bg-brandblue p-3  text-2xl font-bold text-white">
             {ContentData.contentComponentWelcomeHero.title}
           </h1>
           <p className="bg-brandyellow p-3 text-2xl font-bold text-brandblue-800">
             {ContentData.contentComponentWelcomeHero.subtitle}
           </p>
-          <p className="bg-brandgreen p-3 text-base text-white">
+          <p className="line-clamp-4 hidden bg-brandgreen p-3 text-base text-white lg:line-clamp-none lg:block">
             {ContentData.contentComponentWelcomeHero.description}
           </p>
         </div>
       </div>
-      <div
-        id="hero-bottomline"
-        className="absolute bottom-0 w-full bg-brandblue-800"
-      >
+      <div id="hero-bottomline" className="absolute bottom-0 w-full bg-brandblue-800">
         <p className="p-2 text-center text-lg font-bold text-brandyellow">
           {ContentData.contentComponentWelcomeHero.actionText}
         </p>
