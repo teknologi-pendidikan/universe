644c5024c0f975f2ac359fb30a9279e8c8365cfe
vzrenggamani <rengga.prakoso@gmail.com>
2023-07-17 13:55:26 +0700

    feat: gudeiline content and policies

---
+++ apps/oer-center/app/guidelines/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+export default function Page() {
+  return (
+    <main className="container mx-auto pt-44">
+      <h1 className="pb-16 text-6xl font-bold">OER Repository Guidelines</h1>
+      <iframe
+        src="https://drive.google.com/file/d/15ipgVXLOwRDeOAV4Sgv3UfJHpFYIgGJD/preview"
+        width="100%"
+        height="100%"
+        title="OER Repository Guidelines"
+        className="h-[90vh] w-full"
+      ></iframe>
+    </main>
+  )
+}
