0c02c96e3d5c07787355740b7313854f306b3824
vzrenggamani <rengga.prakoso@gmail.com>
2023-07-17 13:42:28 +0700

    feat: add learning content and thematic routes

--- apps/oer-center/app/sitemap.ts
+++ apps/oer-center/app/sitemap.ts
@@ -1,12 +1,36 @@
+import LearningContents from 'data/learning.content.json'
+import ThematicContents from 'data/thematic.content.json'
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = ['', '/learning-content', '/contributors', '/support-us', '/policy', '/guidelines', '/about'].map(
-    (route) => ({
-      url: `https://sumberbelajar.teknologipendidikan.sch.id${route}`,
+  // Learning Content
+  const routesLearningContent = LearningContents.learningcontent.map(
+    (learningContent: LearningContentProperties) => ({
+      url: `https://sumberbelajar.teknologipendidikan.sch.id/learning-content/${learningContent.uuid}`,
       lastModified: new Date().toISOString(),
     }),
   )
 
-  return [...routes]
+  // Thematic Content
+  const routesThematicContent = ThematicContents.thematiclist.map(
+    (thematicContent: ThematicLearningContentProperties) => ({
+      url: `https://sumberbelajar.teknologipendidikan.sch.id/thematic/${thematicContent.uuid}`,
+      lastModified: new Date().toISOString(),
+    }),
+  )
+
+  const routes = [
+    '',
+    '/learning-content',
+    '/contributors',
+    '/support-us',
+    '/policy',
+    '/guidelines',
+    '/about',
+  ].map((route) => ({
+    url: `https://sumberbelajar.teknologipendidikan.sch.id${route}`,
+    lastModified: new Date().toISOString(),
+  }))
+
+  return [...routes, ...routesLearningContent, ...routesThematicContent]
 }
