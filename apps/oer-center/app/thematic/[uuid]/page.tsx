0d054da4439dd426ff1cdec7ab6ca27dd4e06281
vzrenggamani <rengga.prakoso@gmail.com>
2023-07-24 14:46:56 +0700

    fix: more padding on footer

--- apps/oer-center/app/thematic/[uuid]/page.tsx
+++ apps/oer-center/app/thematic/[uuid]/page.tsx
@@ -83,7 +83,7 @@ export default async function Page({
   })
 
   return (
-    <main id="main-content" tabIndex={-1} className="focus:outline-1">
+    <main id="main-content" tabIndex={-1} className="pb-24 focus:outline-1">
       <Image
         src={thematicThumbnail}
         alt="hero"
